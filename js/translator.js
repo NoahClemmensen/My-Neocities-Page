5337a27f72379fd42148684fa9fb379dd023c814
NoahClemmensen <noah0940@edu.sde.dk>
2025-11-20 13:07:48 +0100

    Fixed page moving when using translator

--- js/translator.js
+++ js/translator.js
@@ -1,15 +1,26 @@
+// js
 function toggleTranslator() {
   const button = $("#translator");
   const alienText = $(".alien-text");
   const humanText = $(".human-text");
 
+  let scrollY = window.scrollY;
+  const scrollX = window.scrollX;
+  let buttonTop = button.offset().top;
+  const relativeY = buttonTop - scrollY;
+
   if (button.hasClass("active")) {
-    alienText.show()
-    humanText.hide()
+    alienText.show();
+    humanText.hide();
     button.removeClass("active");
   } else {
-    alienText.hide()
-    humanText.show()
+    alienText.hide();
+    humanText.show();
     button.addClass("active");
   }
+
+  window.requestAnimationFrame(function() {
+    const newButtonTop = button.offset().top;
+    window.scrollTo(scrollX, newButtonTop - relativeY);
+  });
 }
